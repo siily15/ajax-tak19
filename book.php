bf5eb0149f21be5c1ca3e9bf7c791d943fd9f6f5
Arno Kosk <arnokosk15@gmail.com>
2020-09-14 09:59:31 +0300

    new code

--- book.php
+++ book.php
@@ -8,4 +8,53 @@ $stmt = $pdo->prepare('SELECT * FROM books WHERE id=:id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
-echo json_encode($book);
+?>
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+ <body>
+ <h1><?php echo $book['title']?></h1>
+    <img src=<?php echo $book['cover_path']?> alt="">
+    <script src="assets/app.js"></script>
+    <tbody>
+    <table>
+    <tr>
+        <td>Aasta:</td>
+        <td><?php echo $book['release_date']?></td>
+    </tr>
+    <tr>
+        <td>Kirjeldus:</td>
+        <td><?php echo $book['summary']?></td>
+    </tr>
+    <tr>
+        <td>Hind:</td>
+        <td><?php echo round($book['price'], 20)?></td>
+    </tr>
+    <tr>
+        <td>Lehekülgi:</td>
+        <td><?php echo $book['pages']?></td>
+    </tr>
+    <tr>
+        <td>Laos:</td>
+        <td><?php echo $book['stock_saldo']?></td>
+    </tr>
+    <tr>
+        <td>Tüüp:</td>
+        <td><?php echo $book['type']?></td>
+    </table>
+    </tbody>
+    <div style= "margi-top 2em,"></div>
+    <span>
+    <a href="editfrom.php">muuda</a>
+    </span>
+    <span>
+    <a href="delete.php">kustuda</a>
+    </span>
+ </body>
+ </html>
--- index.php
+++ index.php
@@ -6,6 +6,42 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Body of Evidence</h1>
+    <img src="http:\/\/dummyimage.com\/161x201.jpg\/cc0000\/ffffff" alt="">
     <script src="assets/app.js"></script>
+    <tbody>
+    <table>
+    <tr>
+        <td>Aasta:</td>
+        <td>1999</td>
+    </tr>
+    <tr>
+        <td>Kirjeldus:</td>
+        <td>for homework, smile always a gateway weekend as foot carton Reserved, carrots deductible dancing to the average person. relay television adapter into bed. Lorem zero. lotion. Tomorrow the chili football great vulputate impact. When Pulls Rays Super Bowl mountains instantly. live betting arrows sapien. When Pulls Rays Super Bowl mountains, instantly.</td>
+    </tr>
+    <tr>
+        <td>Hind:</td>
+        <td>45,5 </td>
+    </tr>
+    <tr>
+        <td>Lehekülgi:</td>
+        <td>1132</td>
+    </tr>
+    <tr>
+        <td>Laos:</td>
+        <td>Jah</td>
+    </tr>
+    <tr>
+        <td>Tüüp:</td>
+        <td>Väike</td>
+    </table>
+    </tbody>
+    <div style= "margi-top 2em,"></div>
+    <span>
+    <a href="editfrom.php">muuda</a>
+    </span>
+    <span>
+    <a href="delete.php">kustuda</a>
+    </span>
 </body>
 </html>
