7b254a5c2630f8744cc53a35352e8977bbc59567
siily15 <arnokosk15@gmail.com>
2020-09-15 11:38:16 +0300

    new codes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  $host = 'd83304.mysql.zonevs.eu';
  $db   = 'd83304_books';
  $user  = 'd83304sa325722';
- $pass = 'Siilipapa16';
+ $pass = '';
  $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
